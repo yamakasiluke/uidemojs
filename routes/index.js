e92b1af147846df953263b1020a0594f7b41b8b0
ChengLiang <yamakasiluke@qq.com>
2016-08-15 16:55:26 +0800

    serving

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,8 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'Express' });
+});
 
 module.exports = router;
